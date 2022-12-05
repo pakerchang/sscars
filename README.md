c41050a889a3a9b13b3bccda88f28a06f6d83ae8
pakerchang <pakerchang@gmail.com>
2022-12-05 17:27:46 +0800

    feat(Components): #10 Add dep Recoil, member page

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
 ## Clone and research by [car website](http://sscars.com.tw/)
 
 ## dependencies
+
 1. Next@12
 2. Reat@17.0.2
 3. Tailwind@3
 4. Prettier@2.8
 5. Headless-ui@1.7.4
+6. Recoil@0.7.6
 
-## Setup 
+## Setup
 
 ```script
 yarn install
--- components/members/index.js
+++ components/members/index.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { useRecoilValue } from "recoil";
+import { memberDataState } from "store/members";
+import Avatar from "@/members/Avatar";
 
 function Memebers() {
-  return <div>Memebers</div>;
+  const members = useRecoilValue(memberDataState);
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 md:gap-16 text-center">
+      {members?.teamMember !== undefined ? (
+        members.teamMember.map((item) => (
+          <Avatar key={item.name} name={item.name} role={item.role} img={item.img} />
+        ))
+      ) : (
+        <></>
+      )}
+    </div>
+  );
 }
 
 export default Memebers;
